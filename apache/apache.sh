8760eda92abf388e0783eeea8f7a78573028ecf9
stewll <ttvestee@gmail.com>
2023-10-01 18:35:05 +0200

    set WP php7 as default

--- apache/apache.sh
+++ apache/apache.sh
@@ -1,4 +1,10 @@
-# disable the apache2 cache module
+# Enabling apache2 modules and virtual hosts
+a2enmod proxy
+# Enable the apache2 fastcgi module
+a2enmod proxy_fcgi
+
+
+# Disable the apache2 cache module
 a2dismod cache
 # Disable the apache2 cache for the proxy module
 a2dismod cache_disk
